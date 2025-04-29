517282abc12caf81bec5df66337e9f3c3477d2a8
AustinTackett <117427861+AustinTackett@users.noreply.github.com>
2025-04-29 18:06:56 -0500

    Added Win and Game Over Text

--- Assets/Scripts/Accordion.cs
+++ Assets/Scripts/Accordion.cs
@@ -4,12 +4,23 @@ using UnityEngine.InputSystem;
 public class Accordion : MonoBehaviour
 {
     public Healthbar enemyHealthBar;
+    public Crab crab;
+    public GameObject WinText;
+    
+    [HideInInspector] public bool CanAttack = true;
 
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if(Input.GetKeyDown(KeyCode.Space) && CanAttack)
         {
             enemyHealthBar.Health -= 0.1f;
+            enemyHealthBar.Health = Mathf.Max(0, enemyHealthBar.Health);
+        }
+
+        if(enemyHealthBar.Health <= 0)
+        {
+            crab.CanAttack = false;
+            WinText.gameObject.SetActive(true);
         }
     }
 }
--- Assets/Scripts/Crab.cs
+++ Assets/Scripts/Crab.cs
@@ -3,6 +3,10 @@ using UnityEngine;
 public class Crab : MonoBehaviour
 {
     public Healthbar enemyHealthBar;
+    public Accordion accordion;
+    public GameObject GameOverText;
+
+    [HideInInspector] public bool CanAttack = true;
     public float AttackCooldown = 2;
     private float timeLastAttack;
 
@@ -13,11 +17,17 @@ public class Crab : MonoBehaviour
 
     void Update()
     {
-       if(Time.time - timeLastAttack > AttackCooldown)
-       {
-            Debug.Log(Time.time - timeLastAttack);
+        if(Time.time - timeLastAttack > AttackCooldown && CanAttack)
+        {
             timeLastAttack = Time.time;
             enemyHealthBar.Health -= 0.1f;
-       }
+            enemyHealthBar.Health = Mathf.Max(0, enemyHealthBar.Health);
+        }
+
+        if(enemyHealthBar.Health <= 0)
+        {
+            accordion.CanAttack = false;
+            GameOverText.gameObject.SetActive(true);
+        }
     }
 }
